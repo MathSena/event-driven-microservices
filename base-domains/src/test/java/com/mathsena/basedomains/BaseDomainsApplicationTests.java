13b68c0ae0ec8786cb89b1d3947c4ab1d68745f0
MathSena <MathSena07@hotmail.com>
2023-07-17 22:31:12 +0100

    Initial commit

---
+++ base-domains/src/test/java/com/mathsena/basedomains/BaseDomainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathsena.basedomains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDomainsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
